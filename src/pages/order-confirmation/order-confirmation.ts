2694a02cf0a95c177d38f2131f4ba24a7651e3f9
Luccas Calderaro <luccashmc@hotmail.com>
2020-02-22 13:45:57 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -1,3 +1,4 @@
+import { PedidoService } from './../../services/domain/pedido.service';
 import { ClienteService } from './../../services/domain/cliente.service';
 import { ClienteDTO } from './../../models/cliente.dto';
 import { CartService } from './../../services/domain/cart.service';
@@ -22,7 +23,8 @@ export class OrderConfirmationPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
     this.pedido = this.navParams.get('pedido');
   }
 
@@ -49,5 +51,24 @@ export class OrderConfirmationPage {
   total(){
     return this.cartService.total();
   }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+
+      },
+      error=> {
+        if(error.status == 403){
+          this.navCtrl.setRoot('HomePage');
+        }
+      })
+  }
+  
 
 }
