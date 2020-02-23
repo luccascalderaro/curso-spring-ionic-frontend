8b51250409856b785ee7529dff5b43bdbd2877e4
Luccas Calderaro <luccashmc@hotmail.com>
2020-02-23 13:35:23 -0300

    Usando cordova para tirar foto com a camera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,32 +4,37 @@ import { StorageService } from './../../services/storage.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ClienteDTO } from '../../models/cliente.dto';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
-export class ProfilePage {
+
+export class ProfilePage{
 
   cliente: ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
   constructor(public navCtrl: NavController,
-     public navParams: NavParams,
-     public storage: StorageService,
-     public clienteService: ClienteService)
-      {
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService,
+    public camera: Camera
+    ) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
-    if (localUser && localUser.email){
+    if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email).
         subscribe(response => {
           this.cliente = response as ClienteDTO;
           this.getImageIfExists();
-        }, error => { 
-          if(error.status == 403){
+        }, error => {
+          if (error.status == 403) {
             this.navCtrl.setRoot('HomePage');
           }
         });
@@ -38,13 +43,30 @@ export class ProfilePage {
       this.navCtrl.setRoot('HomePage');
     }
   }
-  
-  getImageIfExists(){
+
+  getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
-    .subscribe(response => {
-      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
-    }, 
-    error => {});
+      .subscribe(response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      },
+        error => { });
+  }
+
+  getCameraPicture() {
+    this.cameraOn = true;
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.picture = 'data:image/png;base64,' + imageData;
+      this.cameraOn = false;
+    }, (err) => {
+
+    });
   }
 
 }
