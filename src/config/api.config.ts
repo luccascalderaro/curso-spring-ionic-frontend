61196352b045ba28294d6412893b7e044bcb2b54
Luccas Calderaro <luccashmc@hotmail.com>
2020-02-24 11:25:04 -0300

    Heroku atualizacao endereco login

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://curso-spring-ioniclc.herokuapp.com",
     bucketBaseUrl: "https://lcap0.s3-sa-east-1.amazonaws.com"
 
 }
