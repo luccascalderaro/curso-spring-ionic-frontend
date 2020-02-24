0091d71c08b227eb833b8a5bd310588bda005117
Luccas Calderaro <luccashmc@hotmail.com>
2020-02-24 14:05:15 -0300

    Bugfix - cancelamento da camera ou galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -69,6 +69,7 @@ export class ProfilePage{
       this.picture = 'data:image/png;base64,' + imageData;
       this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false;
 
     });
   }
@@ -87,6 +88,7 @@ export class ProfilePage{
       this.picture = 'data:image/png;base64,' + imageData;
       this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false;
 
     });
   }
